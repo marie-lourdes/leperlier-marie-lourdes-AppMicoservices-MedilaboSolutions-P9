ad889bde0104bfa210889665e7fddd1bd2ddfb20
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-18 20:56:31 +0200

    layer-repository: add interface IpatientRepository with JpaRepository

---
+++ microservice-patients/src/main/java/com/medilabo/microservicepatients/repository/IPatientRepository.java
@@ -0,0 +1,9 @@
+package com.medilabo.microservicepatients.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medilabo.microservicepatients.model.Patient;
+
+public interface IPatientRepository extends JpaRepository<Patient,Long>{
+
+}
