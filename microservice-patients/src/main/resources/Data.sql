4582cb135ac91e907efb19783e6e25557d03a323
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-18 19:31:32 +0200

    add data.sql for default value and testing patients in BDD SQL

---
+++ microservice-patients/src/main/resources/Data.sql
@@ -0,0 +1,16 @@
+use db_testpatient;
+
+/*--------------- DEFAULT VALUES DB testpatient ------------*/
+
+
+INSERT INTO patient(first_name, last_name, genre, adresse, telephone) VALUES
+("Test", "TestNone", "F","1 Brookside St","100-222-3333");
+
+INSERT INTO patient(first_name, last_name, genre, adresse, telephone) VALUES
+("Test", "TestBorderline", "M","2 High St","200-333-4444");
+
+INSERT INTO patient(first_name, last_name, genre, adresse, telephone) VALUES
+("Test", "TestInDanger ", "M","3 Club Road","300-444-5555");
+
+INSERT INTO patient(first_name, last_name, genre, adresse, telephone) VALUES
+("Test", "TestEarlyOnset", "F","4 Valley Dr","400-555-6666");
