287d5a7a70b00771f19acc1da37df2abaf264e17
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-19 11:46:54 +0200

    add User in memory and encode password of USER and ADMIN with BCryptPasswordEncoder in class config AuthentificationWebSecurity

--- microservice-patients/src/main/java/com/medilabo/microservicepatients/config/AuthentificationWebSecurity.java
+++ microservice-patients/src/main/java/com/medilabo/microservicepatients/config/AuthentificationWebSecurity.java
@@ -6,24 +6,43 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AuthentificationWebSecurity {
-	
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(request -> {
 			request.requestMatchers("/patient/**").hasRole("ADMIN");
-			request.requestMatchers(HttpMethod.GET,"/patient/").hasRole("USER");
+			request.requestMatchers(HttpMethod.GET, "/patient/").hasRole("USER");
 			request.requestMatchers("/h2-console/**").permitAll();
 			request.anyRequest().authenticated();
 
-		}).formLogin(Customizer.withDefaults()).oauth2Login(Customizer.withDefaults()).logout((logout) -> logout.permitAll());
-				
-		http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));// desactive le header securité cors pour le path vers la bdd H2
+		}).formLogin(Customizer.withDefaults()).logout((logout) -> logout.permitAll());
+
+		http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));// desactive le header securité cors pour le
+																			// path vers la bdd H2
 		return http.build();
 	}
 
+	@Bean
+	public UserDetailsService users() {
+		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("user")).roles("USER")
+				.build();
+		UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin"))
+				.roles("USER", "ADMIN").build();
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
