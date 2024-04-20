1eb96f0a4a1e4efe2b8e587df8746b435923b44f
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-04-20 13:54:41 +0200

    layer-repository: add avstract methods in interface  IPatientRepository

--- microservice-patients/src/main/java/com/medilabo/microservicepatients/repository/IPatientRepository.java
+++ microservice-patients/src/main/java/com/medilabo/microservicepatients/repository/IPatientRepository.java
@@ -1,9 +1,15 @@
 package com.medilabo.microservicepatients.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.medilabo.microservicepatients.model.Patient;
 
 public interface IPatientRepository extends JpaRepository<Patient,Long>{
-
+	Optional<Patient> addPatient();
+    Patient updatePatient();
+	Optional<Patient> getPatientById();
+	List<Patient> getAllPatients();
 }
