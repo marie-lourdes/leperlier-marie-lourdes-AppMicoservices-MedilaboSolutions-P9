5c61557f67ea268079e757578665a795ebd6739c
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-05 23:34:40 +0200

    initialize and configure properties of  microservice-eureka

---
+++ microservice-eureka/src/test/java/com/medilabo/microserviceeureka/MicroserviceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.microserviceeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
