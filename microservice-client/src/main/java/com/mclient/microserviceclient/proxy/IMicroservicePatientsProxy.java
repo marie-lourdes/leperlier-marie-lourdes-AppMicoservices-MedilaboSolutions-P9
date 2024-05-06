47fc02aec9a0bebb423e983dd2db847e218b33f5
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-06 16:38:08 +0200

    microservice-client: refactor abstract methods signatures of IMicroservicePatientsProxy

--- microservice-client/src/main/java/com/mclient/microserviceclient/proxy/IMicroservicePatientsProxy.java
+++ microservice-client/src/main/java/com/mclient/microserviceclient/proxy/IMicroservicePatientsProxy.java
@@ -3,7 +3,6 @@ package com.mclient.microserviceclient.proxy;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
