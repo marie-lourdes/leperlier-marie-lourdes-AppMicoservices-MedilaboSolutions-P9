f5f40ab79e0f012bbe4702ebb43b55f1572811b7
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-06 13:53:59 +0200

    microservice-client: add model PatientBean

--- microservice-client/src/main/java/com/mclient/microserviceclient/bean/PatientBean.java
+++ microservice-client/src/main/java/com/mclient/microserviceclient/bean/PatientBean.java
@@ -1,5 +1,36 @@
 package com.mclient.microserviceclient.bean;
 
+import com.medilabo.microservicepatients.model.Patient;
+import com.medilabo.microservicepatients.utils.MessageErrorValidationCustomized;
+import com.medilabo.microservicepatients.utils.RegexConstant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
 public class PatientBean {
 
+	private long id;
+	private String prenom;
+	private String nom;
+	private String dateDeNaissance;
+	private String genre;
+	private String adresse;
+	private String telephone;
+
+	public PatientBean() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "ProductBean{" + "id=" + id + ",  prenom='" + prenom + '\'' + ", nom='" + nom + '\''
+				+ ", dateDeNaissance='" + dateDeNaissance + '\'' + ", genre='" + genre + '\'' + ",adresse='" + adresse
+				+ '\'' + ", telephone=" + telephone + '}';
+	}
 }
