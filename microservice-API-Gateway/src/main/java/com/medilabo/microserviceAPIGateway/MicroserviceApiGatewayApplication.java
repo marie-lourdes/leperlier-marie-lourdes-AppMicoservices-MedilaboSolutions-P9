f586d95bad9c5ae80d773b2ae43f4af676adf397
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-06 02:43:41 +0200

    refactor microservice-API-gateway

--- microservice-API-Gateway/src/main/java/com/medilabo/microserviceAPIGateway/MicroserviceApiGatewayApplication.java
+++ microservice-API-Gateway/src/main/java/com/medilabo/microserviceAPIGateway/MicroserviceApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.medilabo.microserviceAPIGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceApiGatewayApplication {
 
 	public static void main(String[] args) {
