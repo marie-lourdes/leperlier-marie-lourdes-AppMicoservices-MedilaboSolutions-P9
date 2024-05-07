d114fdcc0f40cd1605b9e8028bfba87a870b49c3
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-07 14:40:50 +0200

    microservice-client: create HomeController.java and method -route

---
+++ microservice-client/src/main/java/com/mclient/microserviceclient/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.mclient.microserviceclient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/home")
+	public String homePage(Model model) {
+		return "Home";
+	}
+}
