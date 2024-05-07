2573a375d149bd0b3d23f0b16ad32ec5cda7a59a
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-07 17:33:07 +0200

    microservice-client: add method and route HTTP POST @PostMapping('/formPatient')

--- microservice-client/src/main/java/com/mclient/microserviceclient/controller/PatientController.java
+++ microservice-client/src/main/java/com/mclient/microserviceclient/controller/PatientController.java
@@ -35,7 +35,6 @@ public class PatientController {
 			if (result.hasErrors()) {
 				return "FormPatient";
 			}
-	
 			microservicePatientsProxy.createPatient(patient);
 		
         }catch(Exception e) {
