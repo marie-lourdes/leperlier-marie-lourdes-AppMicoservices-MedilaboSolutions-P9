2c2e673fa6fdb12c7f28ff9e5a615d174311c803
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-07 13:32:05 +0200

    microservice-client: create View HTML error404.html and ErrorController.java

---
+++ microservice-client/src/main/java/com/mclient/microserviceclient/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package com.mclient.microserviceclient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+	@GetMapping("/**")
+	public String error(Model model) {
+		return "error404";
+	}
+}
