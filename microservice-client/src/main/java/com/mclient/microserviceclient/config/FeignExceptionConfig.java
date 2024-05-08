b4c9d34f740668817592b53349e7f6c9b8aef038
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-08 18:47:04 +0200

    miroservice-client: create classe configuration  FeignExceptionConfig.java with bean CustomErrorDecoder.java

---
+++ microservice-client/src/main/java/com/mclient/microserviceclient/config/FeignExceptionConfig.java
@@ -0,0 +1,15 @@
+package com.mclient.microserviceclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mclient.microserviceclient.exception.CustomErrorDecoder;
+
+@Configuration
+public class FeignExceptionConfig {
+
+	   @Bean
+	   public CustomErrorDecoder mCustomErrorDecoder(){
+	       return new CustomErrorDecoder();
+	   }
+}
