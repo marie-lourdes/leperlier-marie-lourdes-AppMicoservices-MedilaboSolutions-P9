dfdb762f40a6986c03f0c70977c2e98d94c63389
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-09 13:07:02 +0200

    microservice-API-Gateway: add spring seurity in pom.xml and configuration

--- microservice-API-Gateway/src/main/java/com/medilabo/microserviceAPIGateway/config/AuthenticationWebSecurity.java
+++ microservice-API-Gateway/src/main/java/com/medilabo/microserviceAPIGateway/config/AuthenticationWebSecurity.java
@@ -1,5 +1,20 @@
 package com.medilabo.microserviceAPIGateway.config;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
