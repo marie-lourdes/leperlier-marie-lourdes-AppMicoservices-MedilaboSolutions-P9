90ed50e284fe78251b24b1ed06381849e6448d4c
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-09 16:50:51 +0200

    microservice-client: add  BCryptPasswordEncoder in class FeignAuthConfig

--- microservice-client/src/main/java/com/mclient/microserviceclient/config/FeignAuthConfig.java
+++ microservice-client/src/main/java/com/mclient/microserviceclient/config/FeignAuthConfig.java
@@ -2,15 +2,23 @@ package com.mclient.microserviceclient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import feign.auth.BasicAuthRequestInterceptor;
 
 @Configuration
 public class FeignAuthConfig {
+	
+	public String username= "root";
+	public String password= "rootroot";
 	 @Bean
 	   public BasicAuthRequestInterceptor mBasicAuthRequestInterceptor()
 
 	{
-	      return  new BasicAuthRequestInterceptor("root", "rootroot");
+	      return  new BasicAuthRequestInterceptor(passwordEncoder().encode("root"), passwordEncoder().encode("rootroot"));
 	   }
+		@Bean
+		public BCryptPasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
 }
