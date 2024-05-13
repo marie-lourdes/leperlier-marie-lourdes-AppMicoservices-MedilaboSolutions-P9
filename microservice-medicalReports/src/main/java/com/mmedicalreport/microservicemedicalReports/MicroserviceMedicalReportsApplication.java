b9e85204e95c249faff4adc6e524839973c8f6b6
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-13 14:49:00 +0200

    microservice-medicalReports: create microservice back with MongoDB and eureka discorvery client

---
+++ microservice-medicalReports/src/main/java/com/mmedicalreport/microservicemedicalReports/MicroserviceMedicalReportsApplication.java
@@ -0,0 +1,13 @@
+package com.mmedicalreport.microservicemedicalReports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceMedicalReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceMedicalReportsApplication.class, args);
+	}
+
+}
---
+++ microservice-medicalReports/src/test/java/com/mmedicalreport/microservicemedicalReports/MicroserviceMedicalReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmedicalreport.microservicemedicalReports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMedicalReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
