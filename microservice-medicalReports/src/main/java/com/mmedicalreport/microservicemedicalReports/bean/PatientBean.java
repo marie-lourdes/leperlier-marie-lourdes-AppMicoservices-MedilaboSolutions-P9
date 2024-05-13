bab18aa6a4485e7a621a57c345d6ca1affbd2a45
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-14 00:24:18 +0200

    microservice-medicalReports: implements layer bean creating PatientBean in package bean

--- microservice-medicalReports/src/main/java/com/mmedicalreport/microservicemedicalReports/bean/PatientBean.java
+++ microservice-medicalReports/src/main/java/com/mmedicalreport/microservicemedicalReports/bean/PatientBean.java
@@ -1,5 +1,20 @@
 package com.mmedicalreport.microservicemedicalReports.bean;
 
-public class PatientBean {
+import lombok.Data;
 
+@Data
+public class PatientBean {
+	private Integer id;
+	private String prenom;
+	private String nom;
+
+	public PatientBean() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "ProductBean{" + "id=" + id + ",  prenom='" + prenom + '\'' + ", nom='" + nom + '\''
+				+ ", dateDeNaissance='" +'}';
+	}
 }
