15268bb8ddc11a2ffd3616d40db9f08d80d8c0fe
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-14 14:37:56 +0200

    microservice-medicalReport/layer-controller: create method getPatientByPatient(@PathVariable String namePatient) and route @GetMapping('/rapport-medical/{namePatient}') in MedicalReportController.java

--- microservice-medicalReports/src/main/java/com/mmedicalreport/microservicemedicalReports/controller/MedicalReportController.java
+++ microservice-medicalReports/src/main/java/com/mmedicalreport/microservicemedicalReports/controller/MedicalReportController.java
@@ -3,19 +3,21 @@ package com.mmedicalreport.microservicemedicalReports.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.medilabo.microservicepatients.exceptions.PatientNotFoundException;
+import com.medilabo.microservicepatients.model.Patient;
 import com.mmedicalreport.microservicemedicalReports.bean.PatientBean;
 import com.mmedicalreport.microservicemedicalReports.model.MedicalReport;
 import com.mmedicalreport.microservicemedicalReports.proxy.IMicroservicePatientsProxy;
 import com.mmedicalreport.microservicemedicalReports.service.MedicalReportService;
 
 import jakarta.validation.Valid;
-
-
 
 @RestController
 @RequestMapping("patient")
@@ -46,5 +48,18 @@ public class MedicalReportController {
 			log.error(e.getMessage());
 		}
 		return medicalReportCreated;
+	}
+
+	@GetMapping("/rapport-medical/{namePatient}")
+	public MedicalReport getPatientByPatient(@PathVariable String namePatient) {
+		MedicalReport medicalReportFoundByPatient = new MedicalReport();
+		try {
+			medicalReportFoundByPatient  = medicalReportService.getMedicalReportByNamePatient(namePatient) ;
+
+		} catch (NullPointerException e) {
+			log.error(e.getMessage());
+			//throw new PatientNotFoundException("Patient not found for id " + id);
+		}
+		return medicalReportFoundByPatient;
 	}
 }
