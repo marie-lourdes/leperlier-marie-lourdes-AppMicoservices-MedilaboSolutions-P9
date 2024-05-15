3702c38e8ea37fab150f5b4a4f0ef6df22844569
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-15 14:36:31 +0200

    microservice-client-layer proxy: add IMicroserviceMedicalReportsProxy.java with abstract method and route  List<MedicalReportBean> getPatientByPatId(@PathVariable Integer patId) of microservice-medicalReport

--- microservice-client/src/main/java/com/mclient/microserviceclient/proxy/IMicroserviceMedicalReportsProxy.java
+++ microservice-client/src/main/java/com/mclient/microserviceclient/proxy/IMicroserviceMedicalReportsProxy.java
@@ -2,13 +2,14 @@ package com.mclient.microserviceclient.proxy;
 
 import java.util.List;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.mclient.microserviceclient.bean.MedicalReportBean;
 
-
-/*@FeignClient(name="microservice-API-Gateway")
+@FeignClient(name="microservice-API-Gateway")
 public interface IMicroserviceMedicalReportsProxy {
 	@GetMapping("/rapport-medical/{patId}")
-    List<MedicalReport> getPatientByPatId(@PathVariable Integer patId)
-}*/
+    List<MedicalReportBean> getPatientByPatId(@PathVariable Integer patId);
+}
