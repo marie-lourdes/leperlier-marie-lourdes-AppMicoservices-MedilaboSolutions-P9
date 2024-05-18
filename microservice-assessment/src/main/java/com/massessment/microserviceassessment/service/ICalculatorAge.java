68eecc4d6d7a3b1952f354381eeff33631d36835
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-18 19:34:52 +0200

    microservice-assessment-diabete: add abstract method int calculateAge()  in Interface  ICalculatorAge

--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/ICalculatorAge.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/ICalculatorAge.java
@@ -1,5 +1,7 @@
 package com.massessment.microserviceassessment.service;
 
+import java.time.LocalDate;
+
 public interface ICalculatorAge {
  int calculateAge( LocalDate birthDate,LocalDate currentDate); 
 }
