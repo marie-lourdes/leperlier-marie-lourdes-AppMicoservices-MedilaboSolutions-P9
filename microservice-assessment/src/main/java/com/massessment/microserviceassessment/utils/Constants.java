81652f2a687eca4172d5336b46d88c9970fdf184
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-18 22:53:48 +0200

    microservice-assessment-diabete: create util class Constants.java

---
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.massessment.microserviceassessment.utils;
+
+public class Constants {
+	private static final String MASCULIN= "M";
+    private static final String FEMININ= "F";
+    
+	private Constants(){}
+}
