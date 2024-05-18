ad17a19f627555091f19d444948112793940c5b1
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-18 18:07:50 +0200

    microservice-assessment-layer Proxy: implements code in interface IMicroserviceMedicalReportsProxy with abstract method getPatientByPatId() of microservice-medicalReports

--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/proxy/IMicroserviceMedicalReportsProxy.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/proxy/IMicroserviceMedicalReportsProxy.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.massessment.microserviceassessment.beans.MedicalReportBean;
 
-
 @FeignClient(name = "microservice-API-Gateway")
 public interface IMicroserviceMedicalReportsProxy {
-	/*@GetMapping("/rapport-medical/{patId}")
-	public List<MedicalReportBean> getPatientByPatId(@PathVariable Integer patId);*/
+	@GetMapping("/MICROSERVICE-MEDICALREPORTS/rapport-medical/{patId}")
+	public List<MedicalReportBean> getPatientByPatId(@PathVariable Integer patId);
 }
