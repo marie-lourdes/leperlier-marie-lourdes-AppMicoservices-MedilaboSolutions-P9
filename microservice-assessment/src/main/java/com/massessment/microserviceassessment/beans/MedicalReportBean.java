3ccc35bf8c33546f571bb0e93b38a68e1043c7c0
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-18 18:02:25 +0200

    microservice-assessment-layer bean: implements PatientBean and MedicalReportBean

--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/beans/MedicalReportBean.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/beans/MedicalReportBean.java
@@ -1,5 +1,25 @@
 package com.massessment.microserviceassessment.beans;
 
-public class MedicalReportBean {
+import java.util.ArrayList;
+import java.util.List;
 
+import lombok.Data;
+
+@Data
+public class MedicalReportBean {
+private String id;	
+	
+    private Integer patId;
+
+    private String patient;
+    
+    private List<String> note= new ArrayList<String>(); 
+       
+    public MedicalReportBean (){
+    	
+    }
+    
+    public void addNotes(String line) {
+    	this.note.add(line);
+    }
 }
--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/beans/PatientBean.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/beans/PatientBean.java
@@ -1,5 +1,18 @@
 package com.massessment.microserviceassessment.beans;
 
-public class PatientBean {
+import lombok.Data;
 
+@Data
+public class PatientBean {
+	private Integer id;
+	private String prenom;
+	private String nom;
+	private String dateDeNaissance;
+	private String genre;
+	private String adresse;
+	private String telephone;
+
+	public PatientBean() {
+
+	}
 }
