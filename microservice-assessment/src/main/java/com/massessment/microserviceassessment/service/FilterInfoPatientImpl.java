f5cd355c77d6215d0078622be316bf38e01e65e1
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-18 23:02:02 +0200

    microservice-assessment-diabete-layer Service:implements method filterRiskBorderLine(PatientBean patient) of Interface IFilter in FilterInfoPatientImpl.java

--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/FilterInfoPatientImpl.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/FilterInfoPatientImpl.java
@@ -7,14 +7,15 @@ import org.springframework.stereotype.Component;
 import com.massessment.microserviceassessment.beans.PatientBean;
 import com.massessment.microserviceassessment.proxy.IMicroserviceMedicalReportsProxy;
 import com.massessment.microserviceassessment.proxy.IMicroservicePatientsProxy;
+import com.massessment.microserviceassessment.utils.Constants;
 
 @Component
-public class FilterInfoPatientImpl implements IFilter{
+public class FilterInfoPatientImpl implements IFilter {
 	private IMicroservicePatientsProxy microservicePatientsProxy;
 	private IMicroserviceMedicalReportsProxy microserviceMedicalReportsProxy;
 	private ICalculatorAge calculatorAge;
 
-	public  FilterInfoPatientImpl(IMicroservicePatientsProxy microservicePatientsProxy,
+	public FilterInfoPatientImpl(IMicroservicePatientsProxy microservicePatientsProxy,
 			IMicroserviceMedicalReportsProxy microserviceMedicalReportsProxy) {
 		this.microservicePatientsProxy = microservicePatientsProxy;
 		this.microserviceMedicalReportsProxy = microserviceMedicalReportsProxy;
@@ -28,18 +29,35 @@ public class FilterInfoPatientImpl implements IFilter{
 	}
 
 	@Override
-	public boolean filterRiskBorderLine(PatientBean patient) {
-		boolean isLessThan30years= false;
-		isLessThan30years= calculateAgeOfPatient(patient.getId()) < 30 ? true : false;
-		 return  isLessThan30years;
+	public boolean filterRiskNone(PatientBean patient) {
+		return false;
 	}
+	
+	@Override
+	public boolean filterRiskBorderLine(PatientBean patient) {
+		boolean isLessThan30years = false;
+		isLessThan30years = calculateAgeOfPatient(patient.getId()) < 30 ? true : false;
+		return isLessThan30years;
+	}
+
+	@Override
+	public boolean filterRiskDanger(PatientBean patient) {
+		return false;
+	}
+	
+	@Override
+	public boolean  filterRiskEarlyOnSet(PatientBean patient) {
+		return false;
+	}
+	
 	@Override
 	public String filterSexPatient(PatientBean patient) {
-		String masculin= "M";
-		String feminin= "F";
 		PatientBean patientBean = this.getPatientBean(patient.getId());
-		 ageLessThan30= patientBean.getGenre().equals(masculin)? masculin : feminin ;
+		String isMasculinOrFeminin = patientBean.getGenre().equals(Constants.MASCULIN) ? Constants.MASCULIN
+				: Constants.FEMININ;
+		return isMasculinOrFeminin;
 	}
+
 	private PatientBean getPatientBean(Integer id) {
 		return microservicePatientsProxy.getPatientById(id);
 	}
--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/utils/Constants.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/utils/Constants.java
@@ -1,8 +1,8 @@
 package com.massessment.microserviceassessment.utils;
 
 public class Constants {
-	private static final String MASCULIN= "M";
-    private static final String FEMININ= "F";
+	public static final String MASCULIN= "M";
+    public static final String FEMININ= "F";
     
 	private Constants(){}
 }
