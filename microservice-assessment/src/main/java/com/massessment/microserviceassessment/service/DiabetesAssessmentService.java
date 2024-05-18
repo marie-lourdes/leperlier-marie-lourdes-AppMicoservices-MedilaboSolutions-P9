c2481f95c16ea82ecbc775a8b7824b56f0a66e02
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-18 22:15:00 +0200

    microservice-assessment-diabete-layer Service: create class FilterInfoPatientImpl  and add methods calculateAgeOfPatient(Integer id) and  getPatientBean(Integer id)

--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/DiabetesAssessmentService.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/DiabetesAssessmentService.java
@@ -14,25 +14,8 @@ import lombok.Data;
 @Component
 public class DiabetesAssessmentService {
 
-	private IMicroservicePatientsProxy microservicePatientsProxy;
-	private IMicroserviceMedicalReportsProxy microserviceMedicalReportsProxy;
-	private ICalculatorAge calculatorAge;
 
-	public DiabetesAssessmentService(IMicroservicePatientsProxy microservicePatientsProxy,
-			IMicroserviceMedicalReportsProxy microserviceMedicalReportsProxy) {
-		this.microservicePatientsProxy = microservicePatientsProxy;
-		this.microserviceMedicalReportsProxy = microserviceMedicalReportsProxy;
-		this.calculatorAge = new CalculatorAgeImpl();
 
-	}
-
-	public int calculateAgeOfPatient(Integer id) {
-		PatientBean patientBean = this.getPatientBean(id);
-		return calculatorAge.calculateAge(patientBean.getDateDeNaissance(), LocalDate.now());
-	}
-
-	private PatientBean getPatientBean(Integer id) {
-		return microservicePatientsProxy.getPatientById(id);
-	}
+	
 
 }
--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/FilterInfoPatientImpl.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/FilterInfoPatientImpl.java
@@ -1,5 +1,30 @@
 package com.massessment.microserviceassessment.service;
 
-public class FilterInfoPatientImpl {
+import java.time.LocalDate;
 
+import com.massessment.microserviceassessment.beans.PatientBean;
+import com.massessment.microserviceassessment.proxy.IMicroserviceMedicalReportsProxy;
+import com.massessment.microserviceassessment.proxy.IMicroservicePatientsProxy;
+
+public class FilterInfoPatientImpl {
+	private IMicroservicePatientsProxy microservicePatientsProxy;
+	private IMicroserviceMedicalReportsProxy microserviceMedicalReportsProxy;
+	private ICalculatorAge calculatorAge;
+
+	public  FilterInfoPatientImpl(IMicroservicePatientsProxy microservicePatientsProxy,
+			IMicroserviceMedicalReportsProxy microserviceMedicalReportsProxy) {
+		this.microservicePatientsProxy = microservicePatientsProxy;
+		this.microserviceMedicalReportsProxy = microserviceMedicalReportsProxy;
+		this.calculatorAge = new CalculatorAgeImpl();
+
+	}
+
+	public int calculateAgeOfPatient(Integer id) {
+		PatientBean patientBean = this.getPatientBean(id);
+		return calculatorAge.calculateAge(patientBean.getDateDeNaissance(), LocalDate.now());
+	}
+
+	private PatientBean getPatientBean(Integer id) {
+		return microservicePatientsProxy.getPatientById(id);
+	}
 }
