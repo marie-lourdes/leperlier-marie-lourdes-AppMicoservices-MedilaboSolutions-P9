62cb8277cb991dc32c026ea918a185583f2d9d5a
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-19 00:33:37 +0200

    microservice-assessment-diabete-layer Service: implements methods countSymptomFromMedicalReportNotes(List<String> notes), and setSymptoms() in class Impl CounterTermsMedicalReportNotesImpl.java

--- microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/CounterTermsMedicalReportNotesImpl.java
+++ microservice-assessment/src/main/java/com/massessment/microserviceassessment/service/CounterTermsMedicalReportNotesImpl.java
@@ -43,34 +43,4 @@ public class CounterTermsMedicalReportNotesImpl implements ICounter {
 		}
 		return numberOfSymptoms;
 	}
-	
-	
-/*    switch (symptomType) {
-
-    // Case statements
-    case GOT:
-        System.out.println("Game of Thrones selected");
-        break;
-    case Breakingbad:
-        System.out.println("Breaking Bad selected");
-        break;
-    case Lucifer:
-        System.out.println("Lucifer selected");
-        break;
-    case Boys:
-        System.out.println("Boys selected");
-        break;
-    case Mirzapur:
-        System.out.println("Mirzapur selected");
-        break;
-    case Moneyheist:
-        System.out.println("Money Heist selected");
-        break;
-    default:
-        System.out.println("You are outdated !!!");
-        break;
-    }*/
-	
-	
-
 }
