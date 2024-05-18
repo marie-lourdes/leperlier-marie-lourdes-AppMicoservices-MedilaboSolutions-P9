976d9eb185a2b4e45be8062b4fbb8f35b6606680
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-18 16:52:15 +0200

    fix error git folder microservice-assessment

---
+++ microservice-assessment/src/test/java/com/massessment/microserviceassessment/MicroserviceAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massessment.microserviceassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
