3d0f867e1f8ec3954b3abcf33b698f4913b0a9b0
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-20 16:18:16 +0200

    microservice-config-server: add spring cloud config uri in all configuration of microservices

--- microservice-API-Gateway/src/main/java/com/medilabo/microserviceAPIGateway/config/PropertiesConfig.java
+++
@@ -1,8 +0,0 @@
-package com.medilabo.microserviceAPIGateway.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("mes-configs")
-public class PropertiesConfig {
-
-}
