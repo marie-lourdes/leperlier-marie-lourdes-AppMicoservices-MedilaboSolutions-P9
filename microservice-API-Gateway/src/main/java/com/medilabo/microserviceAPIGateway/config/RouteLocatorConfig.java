94db92bcf8f0264df71b26401ccf5af7f87632de
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-20 22:10:11 +0200

    test sonar cloud properties in microservice-patients

--- microservice-API-Gateway/src/main/java/com/medilabo/microserviceAPIGateway/config/RouteLocatorConfig.java
+++ microservice-API-Gateway/src/main/java/com/medilabo/microserviceAPIGateway/config/RouteLocatorConfig.java
@@ -12,14 +12,5 @@ public class RouteLocatorConfig {
 	@Bean
 	public  DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient dc, DiscoveryLocatorProperties dlp) {
 		return new DiscoveryClientRouteDefinitionLocator(dc,dlp);
-	}
-
-	/*@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder ) {
-		return routeLocatorBuilder.routes().route(r->r.path("/patient/**").filters(f->f.filter((exchange,chain)->{
-			 ServerHttpRequest req = exchange.getRequest();
-			 return chain.filter(exchange.mutate().request(req).build());
-		})).uri("http://localhost:9001/")).build();
-	}*/
-	
+	}	
 }
