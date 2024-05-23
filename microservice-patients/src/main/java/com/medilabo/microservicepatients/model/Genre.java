aa90fa690ec729c8a3647540d59cab9fb9051810
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-05-23 15:14:32 +0200

    microserice-patients-layerRepository: create Interface IGenrePatientRepository

--- microservice-patients/src/main/java/com/medilabo/microservicepatients/model/Genre.java
+++ microservice-patients/src/main/java/com/medilabo/microservicepatients/model/Genre.java
@@ -3,13 +3,19 @@ package com.medilabo.microservicepatients.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name="genre")
+@Data
 public class Genre {
 	
 	@Id
@@ -19,6 +25,11 @@ public class Genre {
 	@Column(name = "genre")
 	private String genre;
 	
+	@OneToMany(
+			   cascade = CascadeType.ALL, 
+			   orphanRemoval = true, 
+			   fetch = FetchType.EAGER)
+				@JoinColumn(name = "id")
 	private List<Patient> patients= new ArrayList<>();
 
 }
